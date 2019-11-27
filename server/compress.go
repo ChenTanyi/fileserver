8b1277f25e62a1855914887a3721222c5fd088d2
ChenTanyi <chentanyi@gmail.com>
2019-11-27 11:34:23 +0800

    feat: add compress processing file

--- server/compress.go
+++ server/compress.go
@@ -3,6 +3,7 @@ package server
 import (
 	"archive/tar"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -18,7 +19,23 @@ func Compress(dir string) error {
 	return TarGzCompress(dir, filename)
 }
 
-func TarGzCompress(inputDirpath string, outputFilename string) error {
+func TarGzCompress(inputDirpath string, outputFilename string) (err error) {
+	processingFile := outputFilename + ".processing"
+	if _, err = os.Stat(processingFile); !os.IsNotExist(err) {
+		if err == nil {
+			return fmt.Errorf("File: %s is already exist", processingFile)
+		}
+		return fmt.Errorf("File: %s stat error, err = %v", processingFile, err)
+	}
+
+	if _, err = os.OpenFile(processingFile, os.O_CREATE|os.O_RDWR, 0755); err != nil {
+		return err
+	}
+
+	defer func() {
+		err = os.Remove(processingFile)
+	}()
+
 	outputFile, err := os.OpenFile(outputFilename, os.O_CREATE|os.O_RDWR, 0755)
 	if err != nil {
 		return err
