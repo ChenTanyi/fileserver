ddffc6ec605c4854d7ca5f7c844793b6a43c8bd3
ChenTanyi <chentanyi@gmail.com>
2020-11-23 00:10:56 +0800

    feat: actions for docker build

--- prebuild.sh
+++ prebuild.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -ex
+
 curdir=`pwd`
 
 cd $(dirname $0)
 
-if [[ -d "server/template" ]]; then
+if [ -d "server/template" ]; then
     go get -v github.com/go-bindata/go-bindata/go-bindata
     cd server
     go-bindata -pkg server template/
