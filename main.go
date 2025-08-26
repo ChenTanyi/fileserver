374d9c7a43cb159ff7a0f646981b593f3e57f185
ChenTanyi <chentanyi@gmail.com>
2025-08-26 19:55:39 +0800

    feat: print local address

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"strings"
 
 	"github.com/chentanyi/fileserver/server"
@@ -16,10 +17,30 @@ func main() {
 	baseUri := flag.String("base", "", "base uri")
 	flag.Parse()
 
+	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	group := router.Group(*baseUri)
 
 	server.NewFileServer(group, "/", *directory)
+
+	// Print local address
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Printf("[WARN] Unable to get local address. err: %s", err.Error())
+	} else {
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if v4 := ip.To4(); v4 != nil {
+				fmt.Printf("Listening at %s\n", v4.String())
+			}
+		}
+	}
 
 	var listenAddr string
 	if strings.ContainsAny(*address, ":") {
